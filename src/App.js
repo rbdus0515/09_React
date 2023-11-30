1d83f594dd09376c9c5284f917ea084e56dee428
rbdus0515 <dnqkd707@gmail.com>
2023-11-30 10:02:03 +0900

    udpate

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Exam1 from './component/Exam1';
 import Exam2 from './component/Exam2';
 import Exam3 from './component/Exam3';
 import Exam4 from './component/Exam4';
+import Exam5 from './component/Exam5';
+import Exam6_1 from './component/Exam6_1';
+import Exam6_2 from './component/Exam6_2';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
       {/* <Exam1 /> */}
       {/* <Exam2 /> */}
       {/* <Exam3 /> */}
-      <Exam4 />
+      {/* <Exam4 /> */}
+      {/* <Exam5 /> */}
+      {/* <Exam6_1 /> */}
+      <Exam6_2 />
 
-      {/** jsx 주석처리 */}
+      {/* jsx 주석처리 */}
     </div>
   );
 }
